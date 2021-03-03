5e396666fdb456d7b65b1e117a58b7d4997b6604
dardan-gashi01 <58520384+dardan-gashi01@users.noreply.github.com>
2021-03-03 19:54:45 +0000

    threads

--- networks/ChatServer/src/com/muc/ServerMain.java
+++ networks/ChatServer/src/com/muc/ServerMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
     public static void main(String[] args){
@@ -11,14 +12,38 @@ public class ServerMain {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             while(true) {
+                System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
-                OutputStream outputSteam = clientSocket.getOutputStream();
-                outputSteam.write("Hello World\n".getBytes());
-                clientSocket.close();
+                System.out.println("Accept connection from " + clientSocket);
+                Thread t = new Thread(){
+                    @Override
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                t.start();
+
             }
         }catch(IOException e){
             e.printStackTrace();
+        }catch (InterruptedException e){
+            e.printStackTrace();
         }
 
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputSteam = clientSocket.getOutputStream();
+        for(int i = 0; i < 10 ; i++){
+            outputSteam.write(("Time now is " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
     }
 }
